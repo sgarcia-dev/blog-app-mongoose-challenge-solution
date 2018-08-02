da226fbd594f7baf55457cf3bda07f2bcc0a67ce
Bryan Spearman <bryanspearman@yahoo.com>
2018-08-02 15:26:52 -0400

    added test folder

---
+++ test/integration-tests.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const faker = require("faker");
+const mongoose = require("mongoose");
+const expect = chai.expect;
+const { BlogPost } = require("../models");
+const { app, runServer, closeServer } = require("../server");
+const { TEST_DATABASE_URL } = require("../config");
+
+chai.use(chaiHttp);
