68f5ea6f5797663aa04747e4fbac3ddb38b85e2e
Bryan Spearman <bryanspearman@yahoo.com>
2018-08-03 09:06:47 -0400

    changed module-exports code

--- server.js
+++ server.js
@@ -150,4 +150,4 @@ if (require.main === module) {
     runServer().catch(err => console.error(err));
 }
 
-module.exports = { runServer, app, closeServer };
+module.exports = { app, runServer, closeServer };
